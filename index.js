13da8257dd610575ced8194e3d5a6864b125cad6
sergio <sergio-armando-lopez@hotmail.com>
2020-01-28 13:33:26 -0700

    Index añadido conc omentario:

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
+//COMENTARIO
+//git
 // found in the LICENSE file.
 // extract from chromium source code by @liuwayong
 (function () {
